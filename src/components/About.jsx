6ebc9bc005a9c6a582983d8e02ea088d9a275704
Pranav <yeolepranav13@example.com>
2026-03-01 13:00:40 +0530

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -56,7 +56,7 @@ const About = () => {
           </p>
 
           <a
-            href="/Pranav_Yeole_Resume.pdf" target="_blank"
+            href="/Pranav_Yeole(Resume).pdf" target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full text-lg font-bold
                        transition-transform duration-300 hover:scale-105"
