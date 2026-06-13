4c2f96bfc70d6c70ca0f6730e8f27f3aa3c4f80d
Pranav Yeole <yeolepranav13@gmail.com>
2026-06-13 22:19:52 +0530

    Add new project images and update project list

--- src/constants.js
+++ src/constants.js
@@ -25,11 +25,15 @@ import postgreLogo from "./assets/tech_logo/postgre.png";
 import renderLogo from "./assets/tech_logo/render-logo.png";
 import sqlLogo from "./assets/tech_logo/sql.png";
 import Calculator from "./assets/ProjectImages/Calculator.png";
+import Cricket from "./assets/ProjectImages/Cricket.png";
+import oxGame from "./assets/ProjectImages/OX Game.png";
+import ToDo from "./assets/ProjectImages/ToDo WebSite.png";
 import logo from "./assets/ProjectImages/logo.png";
 import fletto from "./assets/ProjectImages/Fletto.png";
 import SIBMT from './assets/Education/SIBMTLOGO.jpg';
 import bytco from "./assets/Education/bytco.jfif";
-
+import School from "./assets/Education/Abhinavlogo.png";
+import College from "./assets/Education/CollegeLogo.jpg";
 
 // Skill Type
 export const SkillsInfo = [
@@ -102,33 +106,31 @@ export const education = [
     desc: "I completed my Bachelor's degree in Computer Application (BCA) from Bytco College, Nashik. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. During my bachelor’s degree, I created several websites using HTML, CSS, and JavaScript, and I continue to enhance my skills through continuous learning.",
     degree: "Bachelor of Computer Application(BCA)",
   },
+  {
+    id: 2,
+    img: College,
+    school: "Science Junior College Lohoner",
+    date: "July 2020 - May 2021",
+    grade: "70%",
+    desc: "I completed my class 12 education from Science Junior College, Lohoner under the HSC board, where I studied Physics, Chemistry, and Mathematics (PCM).",
+    degree: "HSC",
+  },
+  {
+    id: 3,
+    img: School,
+    school: "Dr. Kakasaheb Purnapatre Madhyamik Vidyalaya,Chalisgaon",
+    date: "Apr 2015 - March 2016",
+    grade: "78.80%",
+    desc: "I completed my class 10 education from Abhinav School, Chalisgaon, under the SSC board, where I studied Science",
+    degree: "SSC",
+  },
 ];
 
 
 // Project Type
 export const projects = [
-  {
-    id: 0,
-    title: "Calculator",
-    description:
-      "A fully functional calculator web application built using HTML, CSS, and vanilla JavaScript. It supports basic arithmetic operations (addition, subtraction, multiplication, and division) and displays results in real time with clear input/output formatting. The UI is responsive and user-friendly, making it suitable for both desktop and mobile use. Deployed on GitHub Pages for easy online access.",
-    image: Calculator,
-    tags: ["HTML", "CSS", "JavaScript"],
-    github: "https://github.com/PranavYeole03/Calculator",
-    webapp: "https://pranavyeole03.github.io/Calculator/",
-  },
-  {
-      id: 1,
-      title: "ShopFinity (Blinkit Clone)",
-      description:
-        "Shopfinity is a full-stack e-commerce application developed using MongoDB, Express.js, React, and Node.js (MERN stack). The application allows users to browse products, view details, manage cart items, and interact with a responsive, user-friendly interface. The backend handles API requests, data management, and server-side logic, while the frontend delivers a smooth and modern shopping experience. The project is fully deployed and accessible online.",
-      image: logo,
-      tags: ["React JS", "Node.js", "MongoDB", "Express","API"],
-      github: "https://github.com/PranavYeole03/ShopFinity-Full-Stack-MERN-Application-",
-      webapp: "https://shopfinity-full-stack-mern-application-xqcx.onrender.com/",
-  },
-    {
-      id: 2,
+      {
+      id: 0,
       title: "Fletto Food-Delivery-Service",
       description:
         "Fletto is a smart food ordering and delivery platform designed for restaurants, customers, and delivery partners.It simplifies order management with real-time tracking and smooth dashboard experiences for all roles.Restaurant owners can manage menus and orders efficiently, while users enjoy quick and easy ordering.Delivery partners get clear order details and fast confirmation for seamless deliveries..",
@@ -137,4 +139,26 @@ export const projects = [
       github: "https://github.com/PranavYeole03/Food-Delivery-Website",
       webapp: "https://food-delivery-website-1-mqzj.onrender.com/",
     },
+        {
+      id: 1,
+      title: "ShopFinity (Blinkit Clone)",
+      description:
+        "Shopfinity is a full-stack e-commerce application developed using MongoDB, Express.js, React, and Node.js (MERN stack). The application allows users to browse products, view details, manage cart items, and interact with a responsive, user-friendly interface. The backend handles API requests, data management, and server-side logic, while the frontend delivers a smooth and modern shopping experience. The project is fully deployed and accessible online.",
+      image: logo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express","API"],
+      github: "https://github.com/PranavYeole03/ShopFinity-Full-Stack-MERN-Application-",
+      webapp: "https://shopfinity-full-stack-mern-application-xqcx.onrender.com/",
+    },
+    {
+      id: 2,
+      title: "ToDo WebSite",
+      description:
+        "A lightweight ToDo List application created with HTML, CSS, and vanilla JavaScript. This app allows users to add new tasks, mark tasks as complete, and delete tasks with immediate visual updates. It features a clean, user-friendly design and works smoothly across devices. The project is deployed using GitHub Pages for easy access.",
+      image: ToDo,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/PranavYeole03/ToDo",
+      webapp: "https://pranavyeole03.github.io/ToDo/",
+    },
+
+
 ];
